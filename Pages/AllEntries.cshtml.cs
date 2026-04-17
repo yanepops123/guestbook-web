7c21f85262f00896cec34038adce3366ce8e974c
yanepops123 <mrnikym06@gmail.com>
2026-04-17 08:37:55 +0500

    Add delete button to AllEntries page

--- Pages/AllEntries.cshtml.cs
+++ Pages/AllEntries.cshtml.cs
@@ -1,24 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using GuestBook.Web.Models;
 using GuestBook.Web.Services;
- 
+
 namespace GuestBook.Web.Pages
 {
-   public class AllEntriesModel : PageModel
-   {
-       private readonly TableStorageService _table;
- 
-       public IEnumerable<GuestBookEntry> Entries
-           { get; set; } = new List<GuestBookEntry>();
- 
-       public AllEntriesModel(TableStorageService table)
-       {
-           _table = table;
-       }
- 
-       public void OnGet()
-       {
-           Entries = _table.GetAllEntries();
-       }
-   }
+    public class AllEntriesModel : PageModel
+    {
+        private readonly TableStorageService _table;
+
+        public IEnumerable<GuestBookEntry> Entries
+            { get; set; } = new List<GuestBookEntry>();
+
+        public AllEntriesModel(TableStorageService table)
+        {
+            _table = table;
+        }
+
+        public void OnGet()
+        {
+            Entries = _table.GetAllEntries();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(
+            string partitionKey, string rowKey)
+        {
+            await _table.DeleteEntryAsync(partitionKey, rowKey);
+            return RedirectToPage();
+        }
+    }
 }
